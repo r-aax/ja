705e9adf9bd4fa1030d7f3f0fa57eba43d4740f7
Alexey Rybakov <rybakov.aax@gmail.com>
2021-01-12 09:02:08 +0300

    rates 2021

---
+++ pages/jscc-rates-2021/amortization.js
@@ -0,0 +1,324 @@
+// Амортизация вычислительных систем.
+
+//==================================================================================================
+
+// Реализация с распределением амортизации по месяцам.
+// Предусмотрена возможность изменения графика амортизации в произвольной точке времени.
+
+// Хранилище для реализации амортизации.
+//
+// Аргументы:
+//   year - год, с которого начинаем учет амортизации,
+//   month - месяц, с которого начинаем учет амортизации,
+//   months - на какое количество месяцев в принципе распространяется амортизация.
+AmortizationBank = function(year, month, months)
+{
+    this.Year = year;
+    this.Month = month;
+    this.Months = months;
+
+    // Банк состоит из months элементов, в каждом из которых прописаны деньги.
+    // Инициализируем все это просто нулями.
+    this.E = [];
+    for (var i = 0; i < this.Months; i++)
+    {
+        this.E.push(0.0);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Доступ к элементам банка.
+//
+// Результат:
+//   Индекс в банке амортизации.
+AmortizationBank.prototype.Index = function(year, month)
+{
+    var index = (year * 12 + month) - (this.Year * 12 + this.Month);
+
+    // Ловим выход за пределы массива месяцев.
+    if (index >= this.Months)
+    {
+        alert("AmortizationBank.Index : для месяца " +
+              year + ", " + month +
+              "расчет амортизации не предусмотрен.");
+    }
+
+    return index;
+}
+
+// Получение значения амортизации в точке.
+AmortizationBank.prototype.Get = function(year, month)
+{
+    return this.E[this.Index(year, month)];
+}
+
+// Установка значения амортизации в точке.
+AmortizationBank.prototype.Set = function(year, month, val)
+{
+    this.E[this.Index(year, month)] = val;
+}
+
+// Получение суммарной амортизации за год.
+AmortizationBank.prototype.GetYear = function(year)
+{
+    return Array.Range(12).map(m => this.Get(year, m)).Sum();
+}
+
+// Получение суммарной амортизации, начиная с данной точки.
+AmortizationBank.prototype.GetAllFrom = function(year, month)
+{
+    var res = 0.0;
+
+    for (var i = this.Index(year, month); i < this.Months; i++)
+    {
+        res = res + this.E[i];
+    }
+
+    return res;
+}
+
+// Установка фиксированной суммы амортизации во все месяцы, начиная с заданной точки.
+AmortizationBank.prototype.SetAllFrom = function(year, month, val)
+{
+    for (var i = this.Index(year, month); i < this.Months; i++)
+    {
+        this.E[i] = val;
+    }
+}
+
+// Размазывание суммы амортизации начиная с заданной точки на фиктированное количество месяцев.
+AmortizationBank.prototype.Spread = function(year, month, money, months)
+{
+    var quote = money / months;
+
+    for (var i = 0; i < months; i++)
+    {
+        var index = this.Index(year, month) + i;
+
+        // Ловим выход за пределы массива месяцев.
+        if (index >= this.Months)
+        {
+            alert("AmortizationBank.Spread : для месяца " +
+                  year + ", " + month +
+                  "расчет амортизации не предусмотрен.");
+        }
+
+        this.E[index] = quote;
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Получение первого и последнего ненулевого индекса месяца.
+AmortizationBank.prototype.GetFirstNotNullMonthIndex = function()
+{
+    for (var i = 0; i < this.E.length; i++)
+    {
+        if (this.E[i] > 0.0)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+AmortizationBank.prototype.GetLastNotNullMonthIndex = function()
+{
+    for (var i = this.E.length - 1; i >= 0; i--)
+    {
+        if (this.E[i] > 0.0)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+//==================================================================================================
+
+// Строка таблицы амортизации.
+AmortizationLine = function(node_names, comment, date_point, money)
+{
+    this.NodeNames = node_names;
+    this.Comment = comment;
+    this.DatePoint = date_point;
+    this.Money = money;
+
+    // Сразу создаем банк.
+    this.Bank = new AmortizationBank(2012, 0, 300);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Получение индекса даты привязки.
+AmortizationLine.prototype.GetDatePointMonthIndex = function()
+{
+    return this.Bank.Index(this.DatePoint.getFullYear(),
+                           this.DatePoint.getMonth());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Распределение амортизации с некоторого месяца на данное количество месяцев.
+AmortizationLine.prototype.SpreadAmortization = function(from_year, from_month, months)
+{
+    this.Bank.Spread(from_year, from_month, this.Money, months);
+}
+AmortizationLine.prototype.SpreadAmortizationFromThis = function(months)
+{
+    this.SpreadAmortization(this.DatePoint.getFullYear(),
+                            this.DatePoint.getMonth(),
+                            months);
+}
+AmortizationLine.prototype.SpreadAmortizationFromNext = function(months)
+{
+    this.SpreadAmortization(this.DatePoint.getFullYear(),
+                            this.DatePoint.getMonth() + 1,
+                            months);
+}
+AmortizationLine.prototype.SpreadAmortizationFromThisToRefLine = function(ref_line)
+{
+    var index = line.GetDatePointMonthIndex();
+    var ref_index = ref_line.Bank.GetLastNotNullMonthIndex();
+
+    this.SpreadAmortization(this.DatePoint.getFullYear(),
+                            this.DatePoint.getMonth(),
+                            ref_index - index + 1);
+}
+AmortizationLine.prototype.SpreadAmortizationFromNextToRefLine = function(ref_line)
+{
+    var index = line.GetDatePointMonthIndex();
+    var ref_index = ref_line.Bank.GetLastNotNullMonthIndex();
+
+    this.SpreadAmortization(this.DatePoint.getFullYear(),
+                            this.DatePoint.getMonth() + 1,
+                            ref_index - index);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Генерация HTML.
+get_amortization_table_HTML = function(t)
+{
+    var head = "<table border=\"0\" align=\"center\">";
+    var foot = "</table>";
+    var bg = "#DDDDDD";
+    var bg2 = "#F5F5F5";
+    var year_from = 2012;
+    var year_to = 2024;
+
+    head = head + "<tr>";
+    head = head + "<th bgcolor=\"" + bg + "\">узлы</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">договор</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">поставка</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">сумма</th>";
+    for (var i = year_from; i <= year_to; i++)
+    {
+        head = head + "<th bgcolor=\"" + bg + "\">" + i + "</th>";
+    }
+    head = head + "</tr>";
+
+    var fun_add_0_to_digit = function(d) { return (d < 10) ? ("0" + d) : d; }
+
+    var html =
+        t.reduce
+        (
+            function(acc, al)
+            {
+                var res = acc;
+                var names_str = al.NodeNames;
+
+                if (names_str.length > 1)
+                {
+                    names_str = "<i><font color=\"darkgrey\">" + al.NodeNames + "</font></i>";
+                }
+
+                res = res + "<tr>";
+                res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" + names_str + "</td>"
+                res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" + al.Comment + "</td>"
+                res = res + "<td bgcolor=\"" + bg + "\">" +
+                            al.DatePoint.getFullYear() + "-" +
+                            fun_add_0_to_digit(al.DatePoint.getMonth() + 1) + "-" +
+                            fun_add_0_to_digit(al.DatePoint.getDate()) +
+                            "</td>";
+                res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" + al.Money.toLocaleString() + "</td>";
+
+                for (var i = year_from; i <= year_to; i++)
+                {
+                    var bb = (i == 2020) ? "<b><font color=\"indianred\">" : "";
+                    var be = (i == 2020) ? "</font></b>" : "";
+
+                    res = res + "<td bgcolor=\"" + bg2 + "\" align=\"right\">" +
+                                "<font size=\"-2\">" +
+                                bb + al.Bank.GetYear(i).toLocaleString() + be +
+                                "</font></td>";
+                }
+
+                res = res + "</tr>";
+
+                return res;
+            },
+            ""
+        );
+
+    html = html + "<tr>";
+    html = html + "<td bgcolor=\"" + bg + "\">&nbsp;</td>";
+    html = html + "<td bgcolor=\"" + bg + "\">&nbsp;</td>";
+    html = html + "<td bgcolor=\"" + bg + "\">&nbsp;</td>";
+    html = html + "<td bgcolor=\"" + bg + "\">&nbsp;</td>";
+    for (var i = year_from; i <= year_to; i++)
+    {
+        var bb = (i == 2020) ? "<b><font color=\"indianred\">" : "";
+        var be = (i == 2020) ? "</font></b>" : "";
+
+        html = html + "<td bgcolor=\"" + bg + "\" align=\"right\">" +
+                      "<b><font size=\"-2\">" +
+                      bb +
+                      t.map(al => al.Bank.GetYear(i)).Sum().toLocaleString() +
+                      be +
+                      "</font></b></td>";
+
+    }
+    html = html + "</tr>";
+
+    return head + html + foot + "<br><br>";
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Распределить амортизацию между системами.
+distribute_amortization = function(confs, amort)
+{
+    for (var i = 0; i < confs.length; i++)
+    {
+        confs[i].Amort2020 = 0.0;
+    }
+
+    // Ходим по таблице амортизации, вытаскиваем строки и распределяем по системам.
+    for (var i = 0; i < amort.length; i++)
+    {
+        var line = amort[i];
+
+        // Амортизация считается одинаковым образом как для распределения на одну систему,
+        // так и для распределения на несколько систем.
+        var distr_confs = find_calc_nodes_configurations(confs, line.NodeNames);
+        var distr_weights = distr_confs.map(c => c.FullNodeHoursWeight);
+        var distr_weights_sum = distr_weights.Sum();
+        distr_weights = distr_weights.map(w => w / distr_weights_sum);
+        var value = line.Bank.GetYear(2020);
+        for (var j = 0; j < distr_confs.length; j++)
+        {
+            distr_confs[j].Amort2020 = distr_confs[j].Amort2020 + (distr_weights[j] * value);
+        }
+    }
+
+    // Амортизация на узлочас.
+    for (var i = 0; i < confs.length; i++)
+    {
+        confs[i].NodeHourAmort2020 = confs[i].Amort2020 / confs[i].FullNodeHours;
+    }
+}
+
+//==================================================================================================
---
+++ pages/jscc-rates-2021/calc_resources.js
@@ -0,0 +1,364 @@
+// Вычислительные ресурсы.
+
+//==================================================================================================
+
+// Заносим конфигурацию всех узлов:
+//   100k - МВС-100К
+//     tr - Tornado
+//     ps - Petastream
+//     hw - Haswell
+//     bw - Broadwell
+//    knl - Knights Landing
+//     sl - Skylake
+//     cl - Cascade Lake
+
+//==================================================================================================
+
+// Конфигурация вычислительного узла.
+CalcNodeConfiguration = function(is_active, name, descr, usefull, energy)
+{
+    this.IsActive = is_active;
+    this.Name = name;
+    this.Description = descr;
+    this.Usefull = usefull;
+    this.Energy = energy;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Таблица конфигурации вычислительных узлов.
+CalcNodesConfigurationTable = function()
+{
+    /*
+     * [tr]:
+     * https://ark.intel.com/content/www/ru/ru/ark/products/64596/intel-xeon-processor-e5-2690-20m-cache-2-90-ghz-8-00-gt-s-intel-qpi.html
+     * https://technical.city/ru/cpu/Xeon-Phi-7110X#characteristics
+     *
+     * [hw]:
+     * https://ark.intel.com/content/www/us/en/ark/products/81059/intel-xeon-processor-e5-2697-v3-35m-cache-2-60-ghz.html
+     *
+     * [bw]:
+     * https://ark.intel.com/content/www/us/en/ark/products/91768/intel-xeon-processor-e5-2697a-v4-40m-cache-2-60-ghz.html
+     *
+     * [knl]:
+     * https://ark.intel.com/content/www/ru/ru/ark/products/95830/intel-xeon-phi-processor-7290-16gb-1-50-ghz-72-core.html
+     *
+     * [sl]:
+     * https://ark.intel.com/content/www/us/en/ark/products/120495/intel-xeon-gold-6154-processor-24-75m-cache-3-00-ghz.html
+     *
+     * [cl]:
+     * https://ark.intel.com/content/www/ru/ru/ark/products/192481/intel-xeon-platinum-8268-processor-35-75m-cache-2-90-ghz.html
+     */
+
+    var table =
+    [
+        /*
+        new CalcNodeConfiguration(0, "100k", "MVS-100K",        1.00, ( 36.0 / 110) * 2.00),
+        */
+
+        // Опустить энергопотребление, чтобы попасть в цифру.
+        new CalcNodeConfiguration(1,   "tr", "Tornado",         0.50, ((2 * (0.13 + 0.25315)) / 0.7) * 1.2),
+
+        /*
+        new CalcNodeConfiguration(0,   "ps", "Petastream",      1.00, ( 15.0 /   8) * 1.25),
+        */
+
+        new CalcNodeConfiguration(1,   "hw", "Haswell",         1.00, ((2 * 0.145) / 0.7) * 1.06),
+        new CalcNodeConfiguration(1,   "bw", "Broadwell",       1.00, ((2 * 0.145) / 0.7) * 1.06),
+        new CalcNodeConfiguration(1,  "knl", "Knights Landing", 1.00, ((0.245)     / 0.7) * 1.06),
+        new CalcNodeConfiguration(1,   "sl", "Skylake",         1.00, ((2 * 0.200) / 0.7) * 1.06),
+        new CalcNodeConfiguration(1,   "cl", "Cascade Lake",    1.00, ((2 * 0.205) / 0.7) * 1.06)
+    ];
+
+    // Отбираем только разрешенные конфигурации.
+    table = table.filter(function(conf) { return conf.IsActive; });
+
+    return table;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Получение конфигурации по имени.
+find_calc_node_configuration = function(table, name)
+{
+    var list = table.filter(c => c.Name == name);
+
+    if (list.length != 1)
+    {
+        alert("get_calc_node_configuration : проблемы с получением конфигурации по имени");
+    }
+
+    return list[0];
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Получение списка конфигураций.
+find_calc_nodes_configurations = function(table, names)
+{
+    return names.map(n => find_calc_node_configuration(table, n));
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Сгенерировать HTML таблицы конфигураций вычислительных узлов.
+get_calc_nodes_configuration_table_HTML = function(t)
+{
+    var head = "<table border=\"0\" align=\"center\">";
+    var foot = "</table>";
+    var bg = "lightsteelblue";
+    var bg_money = "lightgreen";
+
+    head = head + "<tr>";
+    head = head + "<th bgcolor=\"" + bg + "\">узел</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">описание</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">полезность</th>";
+    head = head + "<th bgcolor=\"" + bg + "\">энергия</th>";
+    if (t[0].FullNodeHours != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg + "\">узлочасы</th>";
+        head = head + "<th bgcolor=\"" + bg + "\">вес</th>";
+    }
+    if (t[0].Amort2020 != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg + "\">ам. 2020</th>";
+        head = head + "<th bgcolor=\"" + bg_money + "\">ам. у*ч</th>";
+    }
+    if (t[0].EnergyCost != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg_money + "\">ээ. у*ч</th>";
+    }
+    if (t[0].RepairCost != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg_money + "\">рм. у*ч</th>";
+    }
+    if (t[0].OtherCost != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg_money + "\">др. у*ч</th>";
+    }
+    if (t[0].SumCost != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg_money + "\">вс. у*ч</th>";
+    }
+    if (t[0].Rate != undefined)
+    {
+        head = head + "<th bgcolor=\"" + bg_money + "\">тариф</th>";
+    }
+    head = head + "</tr>";
+
+    var html =
+        t.reduce
+        (
+            function(acc, conf)
+            {
+                var res = acc +
+                          "<tr>" +
+                          "<td bgcolor=\"" + bg + "\" align=\"right\">" +
+                          conf.Name +
+                          "</td>" +
+                          "<td bgcolor=\"" + bg + "\">" +
+                          conf.Description +
+                          "</td>" +
+                          "<td bgcolor=\"" + bg + "\">" +
+                          conf.Usefull +
+                          "</td>" +
+                          "<td bgcolor=\"" + bg + "\">" +
+                          conf.Energy.toFixed(3) +
+                          "</td>";
+
+                if (conf.FullNodeHours != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" +
+                          conf.FullNodeHours.toLocaleString() + "</td>";
+                    res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" +
+                          conf.FullNodeHoursWeight.toFixed(3) + "</td>";
+                }
+
+                if (conf.Amort2020 != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" +
+                          conf.Amort2020.toLocaleString() + "</td>";
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.NodeHourAmort2020.toLocaleString() + "</td>";
+                }
+
+                if (conf.EnergyCost != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.EnergyCost.toLocaleString() + "</td>";
+                }
+
+                if (conf.RepairCost != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.RepairCost.toLocaleString() + "</td>";
+                }
+
+                if (conf.OtherCost != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.OtherCost.toLocaleString() + "</td>";
+                }
+
+                if (conf.SumCost != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.SumCost.toLocaleString() + "</td>";
+                }
+
+                if (conf.Rate != undefined)
+                {
+                    res = res + "<td bgcolor=\"" + bg_money + "\" align=\"right\">" +
+                          conf.Rate.toLocaleString() + "</td>";
+                }
+
+                res = res + "</tr>";
+
+                return res;
+            },
+            ""
+        );
+
+    return head + html + foot + "<br><br>";
+}
+
+//==================================================================================================
+
+// Расписание функционирования вычислительного сегмента.
+CalcSegmentSchedule = function(node_name, nodes_count, months_activity)
+{
+    this.NodeName = node_name;
+    this.NodesCount = nodes_count;
+    this.MonthsActivity = months_activity;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Сгенерировать HTML конфигурации вычислительного поля.
+get_calc_segments_schedule_table_HTML = function(t)
+{
+    var head = "<table border=\"0\" align=\"center\">";
+    var foot = "</table>";
+    var months = [ "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек" ];
+    var bg = "#DDDDDD";
+
+    head = head + "<tr>" +
+                  "<th bgcolor=\"" + bg + "\"узлы</th>" +
+                  "<th bgcolor=\"" + bg + "\">кол-во</th>";
+    for (var i = 0; i < months.length; i++)
+    {
+        head = head + "<th bgcolor=\"" + bg + "\">" + months[i] + "</th>";
+    }
+    head = head + "</tr>";
+
+    var html =
+        t.reduce
+        (
+            function(acc, sh)
+            {
+                var res = acc;
+
+                res = res + "<tr>";
+                res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" + sh.NodeName + "</td>";
+                res = res + "<td bgcolor=\"" + bg + "\" align=\"right\">" + sh.NodesCount + "</td>";
+                for (var i = 0; i < months.length; i++)
+                {
+                    var loc_bg = "lightgreen";
+
+                    if (!sh.MonthsActivity[i])
+                    {
+                        loc_bg = "salmon";
+                    }
+
+                    res = res + "<td bgcolor=\"" + loc_bg + "\">&nbsp;</td>";
+                }
+                res = res + "</tr>";
+
+                return res;
+            },
+            ""
+        );
+
+    return head + html + foot + "<br><br>";
+}
+
+//==================================================================================================
+
+// Подсчет полного количества узлочасов для узлов каждого типа.
+calculate_full_node_hours = function(confs, scheds)
+{
+    var mdays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    for (var i = 0; i < confs.length; i++)
+    {
+        var conf = confs[i];
+        var name = conf.Name;
+
+        // Достаем из расписаний все строки, связанные с конкретной конфигураций.
+        var cur_sheds = scheds.filter(function(sh) { return sh.NodeName == name; });
+
+        // Считаем полное количество узлочасов.
+        var full_node_days =
+            cur_sheds.map
+            (
+                function(sh)
+                {
+                    // Количество дней берем с маской по месяцам.
+                    var days = JA.Func.ZipWith(sh.MonthsActivity,
+                                                mdays,
+                                                function(x, y) { return x * y; });
+
+                    return sh.NodesCount * days.Sum();
+                }
+            ).Sum();
+
+        conf.FullNodeHours = full_node_days * 24;
+    }
+
+    // А теперь добавим вес.
+    var total_node_hours = confs.map(function(c) { return c.FullNodeHours; }).Sum();
+    confs.forEach(function(c) { c.FullNodeHoursWeight = c.FullNodeHours / total_node_hours; });
+}
+
+//==================================================================================================
+
+// Вычисление затрат на энергию.
+calculate_energy_costs = function(confs)
+{
+    var kwh = 4.46;
+    var kwm = 734.0;
+    var tkwh = kwh + kwm / (30.0 * 24.0);
+    var calc_energy_cost = function(e) { return e * tkwh; };
+
+    confs.forEach(c => c.EnergyCost = calc_energy_cost(c.Energy));
+}
+
+// Вычисление затрат на ремонт.
+calculate_repair_costs = function(confs)
+{
+    confs.forEach(c => c.RepairCost = 0.0);
+    confs.filter(c => c.Name == "hw")[0].RepairCost = 175000.0 / (365.0 * 24.0);
+    confs.filter(c => c.Name == "bw")[0].RepairCost = 150000.0 / (365.0 * 24.0);
+}
+
+// Другие затраты.
+calculate_other_costs = function(confs, tot)
+{
+    var ks = confs.map(c => c.FullNodeHours * c.Usefull);
+    var sum_ks = ks.Sum();
+    var ks = ks.map(k => k / sum_ks);
+
+    for (var i = 0; i < confs.length; i++)
+    {
+        confs[i].OtherCost = tot * ks[i] / confs[i].FullNodeHours;
+    }
+}
+
+// Суммирование всех затрат.
+summarize_all_costs = function(confs)
+{
+    confs.forEach(c => c.SumCost = c.NodeHourAmort2020 + c.EnergyCost + c.RepairCost + c.OtherCost);
+    confs.forEach(c => c.Rate = c.SumCost * 1.15);
+}
+
+//==================================================================================================
