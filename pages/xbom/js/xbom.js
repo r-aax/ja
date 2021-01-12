f6fd03a353f500bb3f27e1d34c364da3a177b98f
Alexey Rybakov <rybakov.aax@gmail.com>
2021-01-12 09:01:51 +0300

    xbom

--- pages/xbom/js/xbom.js
+++ pages/xbom/js/xbom.js
@@ -78,13 +78,27 @@ Technology.prototype.Find = function(operation)
 {
     var operations = this.Operations.filter(x => x.Name == operation.Name);
 
-    return operations[0];
+    if (operations.length > 0)
+    {
+        return operations[0];
+    }
+    else
+    {
+        return undefined;
+    }
 }
 Technology.prototype.FindForResult = function(result)
 {
     var operations = this.Operations.filter(x => x.Result.Name == result.Name);
 
-    return operations[0];
+    if (operations.length > 0)
+    {
+        return operations[0];
+    }
+    else
+    {
+        return undefined;
+    }
 }
 Technology.prototype.HTML = function()
 {
@@ -95,6 +109,35 @@ Technology.prototype.HTML = function()
                                       + "</td><td>" + o.Duration
                                       + "</td><td>" + o.Result.Name).MergeStrings()
            + "</table>";
+}
+
+//==================================================================================================
+
+SupplyTableLine = function(item, time, quantity)
+{
+    this.Item = item;
+    this.Time = time;
+    this.Quantity = quantity;
+}
+SupplyTable = function()
+{
+    this.A = [];
+}
+SupplyTable.prototype.Add = function(item, time, quantity)
+{
+    this.A.push(new SupplyTableLine(item, time, quantity));
+}
+SupplyTable.prototype.Get = function(time)
+{
+    return this.A.filter(line => line.Time == time);
+}
+SupplyTable.prototype.Remove = function(time)
+{
+    this.A = this.A.filter(line => line.Time != time);
+}
+SupplyTable.prototype.TotalQuantity = function(item)
+{
+    return this.A.filter(line => line.Item.Name == item.Name).map(line => line.Quantity).Sum();
 }
 
 //==================================================================================================
@@ -110,14 +153,20 @@ BOM = function(item, stock, technology)
 }
 BOM.prototype.Expand = function()
 {
-    if (this.Item.IsPrimary())
-    {
-        ;
-    }
-    else if (this.Item.IsSecondary())
-    {
+    //if (this.Item.IsPrimary())
+    //{
+    //    ;
+    //}
+    //else if (this.Item.IsSecondary())
+    //{
         // Find operation.
         var operation = this.Technology.FindForResult(this.Item);
+
+        if (operation == undefined)
+        {
+            return;
+        }
+
         this.Operation = operation;
 
         // Fill children.
@@ -132,11 +181,11 @@ BOM.prototype.Expand = function()
             // Expand this new bom.
             bom.Expand();
         }
-    }
-    else
-    {
-        alert("Uknown type of item");
-    }
+    //}
+    //else
+    //{
+    //    alert("Uknown type of item");
+    //}
 }
 BOM.prototype.HTML = function(val = 1)
 {
@@ -171,10 +220,42 @@ BOM.prototype.HTML = function(val = 1)
 // Симуляция производства.
 BOM.prototype.SimulateProduction = function()
 {
-    this.RecProduce();
+    var max_steps = 30;
+    var current_time = 0;
+    var supply_table = new SupplyTable();
+
+    // Надо симулировать производство пока не произведено изделие.
+    while (this.Stock.Find(this.Item).Quantity < 1)
+    {
+        if (this.Stock.Find(this.Item).Quantity + supply_table.TotalQuantity(this.Item) < 1)
+        {
+            this.RecProduce(current_time, supply_table);
+        }
+
+        max_steps--;
+
+        if (max_steps == 0)
+        {
+            break;
+        }
+
+        current_time++;
+        // Корректировка supply_table.
+        var on_time = supply_table.Get(current_time);
+        for (var i = 0; i < on_time.length; i++)
+        {
+            //alert(on_time[i].Quantity);
+            this.Stock.Find(on_time[i].Item).Quantity += on_time[i].Quantity;
+
+            alert("Finish to produce : " + on_time[i].Item.Name + " (" + on_time[i].Item.Description + ") t = " + current_time);
+        }
+        supply_table.Remove(current_time);
+    }
 }
-BOM.prototype.ProduceSecondaryItem = function()
+BOM.prototype.ProduceSecondaryItem = function(current_time, supply_table)
 {
+    alert("Begin to produce : " + this.Item.Name + " (" + this.Item.Description + ") t = " + current_time);
+
     if (this.Item.IsSecondary())
     {
         var operation = this.Operation;
@@ -187,12 +268,10 @@ BOM.prototype.ProduceSecondaryItem = function()
             item_in_stock.Quantity -= item_in_operation.Quantity;
         }
 
-        var result_in_stock = this.Stock.Find(this.Item);
-
-        result_in_stock.Quantity += 1;
+        supply_table.Add(this.Item, current_time + operation.Duration, 1);
     }
 }
-BOM.prototype.RecProduce = function()
+BOM.prototype.RecProduce = function(current_time, supply_table)
 {
     if (this.Item.IsSecondary())
     {
@@ -200,13 +279,23 @@ BOM.prototype.RecProduce = function()
 
         for (var i = 0; i < this.Children.length; i++)
         {
-            while (this.Stock.Find(this.Children[i].Item).Quantity < operation.SourceItems[i].Quantity)
+            if (this.Stock.Find(this.Children[i].Item).Quantity + supply_table.TotalQuantity(this.Children[i].Item) < operation.SourceItems[i].Quantity)
             {
-                this.Children[i].RecProduce();
+                this.Children[i].RecProduce(current_time, supply_table);
+
+                //return;
             }
         }
 
-        this.ProduceSecondaryItem();
+        for (var i = 0; i < this.Children.length; i++)
+        {
+            if (this.Stock.Find(this.Children[i].Item).Quantity < operation.SourceItems[i].Quantity)
+            {
+                return;
+            }
+        }
+
+        this.ProduceSecondaryItem(current_time, supply_table);
     }
 }
 
